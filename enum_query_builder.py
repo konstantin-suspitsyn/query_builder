f3ca19a55e482be68ee46a4daa535a5feec1ed08
konstantin_s <konstantin.suspitsyn@gmail.com>
2023-12-28 20:44:34 +0300

    Update Enums

--- enum_query_builder.py
+++ enum_query_builder.py
@@ -17,6 +17,40 @@ class TomlTableTableTypeFieldPossibleValues(enum.Enum):
     """
     All possible values for TomlTablesMandatoryFields.TABLE_TYPE field
     """
-
     DATA = "data"
     DIMENSION = "dimension"
+
+
+class TomlTableFieldMandatoryProperties(enum.Enum):
+    """
+    All mush-have properties for fields
+    """
+    TYPE = "type"
+    SHOW = "show"
+
+
+class TomlTableCalculationFieldMandatoryProperties(enum.Enum):
+    """
+    All mush-have properties for fields
+    """
+    CALCULATION = "calculation"
+    WHERE = "where"
+    FACT_MUST_JOIN_ON = "fact_must_join_on"
+
+
+class TomlMandatoryJoins(enum.Enum):
+    """
+    All mandatory properties for toml in joins folder
+    """
+    FIRST_TABLE = "first_table"
+    SCHEMA = "schema"
+    DATABASE = "database"
+    SECOND_TABLE = "second_table"
+
+
+class TomlMandatoryJoinsForSecondTable(enum.Enum):
+    """
+    Mandatory fields for joins
+    """
+    HOW = "how"
+    ON = "on"
