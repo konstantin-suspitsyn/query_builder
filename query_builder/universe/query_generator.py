d671d676b0740d6a7a6e6957c6921526b09d35c5
konstantin_s <konstantin.suspitsyn@gmail.com>
2024-01-18 23:02:34 +0300

    Added __get_dimension_table_that_connects_with_others

--- query_builder/universe/query_generator.py
+++ query_builder/universe/query_generator.py
@@ -1,3 +1,4 @@
+from enum_query_builder import FrontendTypeFields
 from query_builder.utils.data_types import FieldsForQuery
 from query_builder.utils.enums_and_field_dicts import TableTypes
 from query_builder.utils.utils import join_on_to_string
@@ -40,8 +41,14 @@ class QueryGenerator:
 
         if len(selected_objects.get_fact_tables()) == 1:
             from_table = list(selected_objects.get_fact_tables())[0]
+
+            # Check if all joins exist
+            for dimension_table in selected_objects.get_dimension_tables():
+                if not self.joins.has_join(list(selected_objects.get_fact_tables())[0], dimension_table):
+                    raise RuntimeError("No join")
         elif len(selected_objects.get_dimension_tables()) > 1:
-            from_table = list(selected_objects.get_dimension_tables())[0]
+
+            from_table = self.__get_dimension_table_that_connects_with_others(selected_objects.get_dimension_tables())
 
         # Get all tables needed for query
         join_tables.update(selected_objects.get_fact_tables())
@@ -60,9 +67,38 @@ class QueryGenerator:
                 calculation.update(selected_objects[key.value][table]["calculations"])
                 where.update(selected_objects[key.value][table]["where"])
 
+        where.update(selected_objects[FrontendTypeFields.WHERE.value])
+        calculation.update(selected_objects[FrontendTypeFields.CALCULATIONS.value])
+
         query = self.__generate_sql_text_for_one_data_table(select, calculation, where, from_table, join_tables)
 
         return query
+
+    def generate_select_for_multiple_data_tables(self, selected_objects: FieldsForQuery) -> str:
+        """
+        Generates CTE queries with multiple data tables
+        :param selected_objects:
+        :return:
+        """
+        dimension_tables: set = selected_objects.get_dimension_tables()
+        fact_tables: set = selected_objects.get_fact_tables()
+
+        # Fast check if all joins exist
+        for fact_table in fact_tables:
+            if len(selected_objects[TableTypes.DATA.value][fact_table]["select"]) > 0:
+                raise RuntimeError("Select on fact join")
+
+            for dimension_table in dimension_tables:
+
+                if not self.joins.has_join(fact_table, dimension_table):
+                    raise RuntimeError("No join")
+
+        # For CTE we should define which fields will be used for join
+        # If for every fact table, joined dimension table has the same field(s)
+        #   Those fields should be used for later join of two fact fields
+        # If not then fact fields will be used
+
+        pass
 
     def __generate_sql_text_for_one_data_table(self, select: set, calculation: set, where: set,
                                                from_table: str, join_tables: set) -> str:
@@ -113,3 +149,24 @@ class QueryGenerator:
             query += select_query
 
         return query
+
+    def __get_dimension_table_that_connects_with_others(self, dimension_tables: set):
+        """
+        Selects dimension table that has joins with all others dimension tables in set
+        :param dimension_tables: set of dimension tables
+        :return: string with name of tables or raises an error
+        """
+        if len(dimension_tables) == 1:
+            return dimension_tables.pop()
+
+        for from_table in dimension_tables:
+
+            for join_table in dimension_tables:
+                if from_table == join_table:
+                    continue
+                if not self.joins.has_join(from_table, join_table):
+                    break
+
+            return from_table
+
+        raise RuntimeError("No join")
