add26ce69ef70b9e947493bd69ae42d7784a52fc
konstantin_s <konstantin.suspitsyn@gmail.com>
2024-01-28 22:29:34 +0300

    Added Basic User Auth with SQLAlchemy

--- comrade_wolf/auth.py
+++ comrade_wolf/auth.py
@@ -1,11 +1,13 @@
 import functools
 
+import sqlalchemy
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from comrade_wolf.db import get_db
+from comrade_wolf.database import init_db, db_session
+from comrade_wolf.models import User
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -15,8 +17,9 @@ def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        db = get_db()
         error = None
+
+        init_db()
 
         if not username:
             error = 'Username is required.'
@@ -25,12 +28,11 @@ def register():
 
         if error is None:
             try:
-                db.cursor().execute(
-                    "INSERT INTO query_builder.public.user (username, password) VALUES (%s, %s)",
-                    (username, generate_password_hash(password)),
-                )
-                db.commit()
-            except db.IntegrityError:
+                user = User(username=username, password=generate_password_hash(password))
+                db_session.add(user)
+                db_session.commit()
+
+            except sqlalchemy.exc.IntegrityError:
                 error = f"User {username} is already registered."
             else:
                 return redirect(url_for("auth.login"))
@@ -40,28 +42,27 @@ def register():
     return render_template("auth/register.html")
 
 
+#
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        db = get_db()
+
+        init_db()
         error = None
-        with db.cursor() as cursor:
-            cursor.execute(
-                "SELECT id, username, password FROM query_builder.public.user WHERE username = %s", (username)
-            )
-            user = cursor.fetchone()
-            print(user)
+
+        user = User.query.filter_by(username=username).first()
+        print(user)
 
         if user is None:
             error = 'Incorrect username.'
-        elif not check_password_hash(user[2], str(password)):
+        elif not check_password_hash(user.get_password(), str(password)):
             error = 'Incorrect password.'
 
         if error is None:
             session.clear()
-            session['user_id'] = user
+            session['user_id'] = user.get_id()
             return redirect(url_for('hello'))
 
         flash(error)
@@ -76,13 +77,11 @@ def load_logged_in_user():
     if user_id is None:
         g.user = None
     else:
-        with get_db().cursor() as cursor:
-            print(user_id)
-            g.user = cursor.execute(
-                "SELECT * FROM query_builder.public.user WHERE id = %s", (user_id[0],)
-            ).fetchone()
 
-        print(g.user)
+        user = User.query.filter_by(id=user_id).first()
+
+        g.user = user
+
 
 
 @bp.route('/logout')
--- comrade_wolf/models.py
+++ comrade_wolf/models.py
@@ -5,15 +5,18 @@ from comrade_wolf.database import Base
 class User(Base):
     __tablename__ = "user"
     id = Column(Integer, primary_key=True)
-    name = Column(String(50), unique=True)
+    username = Column(String(50), unique=True)
     password = Column(String(256), unique=False)
 
-    def __init__(self, name=None, password=None):
-        self.name = name
+    def __init__(self, username=None, password=None):
+        self.username = username
         self.password = password
 
     def __repr__(self):
-        return f'<User {self.name!r}>'
+        return f'<User {self.username!r}>'
 
     def get_password(self):
         return self.password
+
+    def get_id(self):
+        return self.id
